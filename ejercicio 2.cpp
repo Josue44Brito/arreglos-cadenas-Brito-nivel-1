d962f49f6fa2fc38ab67fc115bca24cb519d0771
Josue.Brito <josuemichael44@gmail.com>
2025-06-22 12:55:46 -0500

    2

---
+++ ejercicio 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string original[5], invertido[5];
+
+    for(int i = 0; i < 5; i++) {
+        cout << "Ingresa una palabra: ";
+        cin >> original[i];
+    }
+
+    for(int i = 0; i < 5; i++) {
+        invertido[i] = original[4 - i];
+    }
+
+    cout << "Vector invertido:\n";
+    for(int i = 0; i < 5; i++) {
+        cout << invertido[i] << endl;
+    }
+    return 0;
+}
